ef70c19c665b9d703b4500a53d57c4e4653e39e2
andpdw <andrew.n.harding@icloud.com>
2026-09-15 23:09:35 -0600

    Added the constructor, addPiece, getPiece and resetBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,10 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPiece[][] board;
+
     public ChessBoard() {
-        
+        board = new ChessPiece[8][8];
     }
 
     /**
@@ -19,7 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getColumn()][position.getRow()] = piece;
     }
 
     /**
@@ -30,7 +32,11 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        if (board[position.getColumn()][position.getRow()] == null) {
+            return null;
+        } else {
+            return board[position.getColumn()][position.getRow()];
+        }
     }
 
     /**
@@ -38,6 +44,6 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        board = new ChessPiece[8][8];
     }
 }
